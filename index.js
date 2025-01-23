29c012cf42bf9e0c7183f64f1c7372e8b75a1122
NishantSinghhhhh <nishantsingh_230137@aitpune.edu.in>
2025-01-23 15:53:49 +0530

    cores
    Signed-off-by: NishantSinghhhhh <nishantsingh_230137@aitpune.edu.in>

--- index.js
+++ index.js
@@ -5,50 +5,36 @@ import mongoose from 'mongoose';
 import resultRoute from './Routes/ResultRoute.js';
 import dotenv from 'dotenv';
 import DataToJson from './Routes/DataToJson.js';
-import Verification from "./Routes/Verification.js"
+import Verification from './Routes/Verification.js';
 
 dotenv.config(); // Load environment variables
 
-const PORT = process.env.PORT || 7009;
 const app = express();
 app.use(cors());
-// Middleware setup
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-// CORS configuration
-// app.use(cors({
-//     origin: process.env.BASE_URL, // Ensure this matches your frontend URL
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true
-// }));
-
-// Handle preflight requests
-app.options('*', cors()); // Enable CORS for all preflight OPTIONS requests
-
+// MongoDB Connection
 const mongoURI = process.env.MONGO_CONN;
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+    .connect(mongoURI)
     .then(() => console.log('MongoDB connection established successfully'))
     .catch(err => console.error('Error connecting to MongoDB:', err));
 
+// Routes
 app.get('/', (req, res) => {
-    res.json("PONG");
+    res.json('PONG');
 });
-
-
 app.use('/result', resultRoute);
 app.use('/datatoJson', DataToJson);
 app.use('/verification', Verification);
 
+// Error Handling Middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.setHeader('Access-Control-Allow-Origin', process.env.BASE_URL); // Ensure this matches frontend URL
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.status(500).json({ message: 'An error occurred!' });
     next();
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Export for Vercel Deployment
+export default app;
